1e159b6833b2fbda58e23eab803472bc42a21e84
Sblvsk <smr.kolesnikov@gmail.com>
2023-02-15 14:10:21 +0600

    Category views

--- core/mainapp/views.py
+++ core/mainapp/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import ListView, CreateView, DetailView, DeleteView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView
 from django.urls import reverse_lazy
+
+from .forms import ProductForm, ProductCategoryForm
 from .models import Product, ProductCategory
 
 
@@ -18,14 +20,8 @@ class ProductListView(ListView):
 
 class ProductCreateView(CreateView):
     model = Product
+    form_class = ProductForm
     template_name = 'product_form.html'
-    fields = ['name', 'description', 'price', 'category']
-    success_url = reverse_lazy('product_list')
-
-
-class ProductDeleteView(DeleteView):
-    model = Product
-    template_name = 'product_confirm_delete.html'
     success_url = reverse_lazy('product_list')
 
 
@@ -34,3 +30,23 @@ class ProductUpdateView(UpdateView):
     template_name = 'product_form.html'
     fields = ['name', 'description', 'price', 'category']
     success_url = reverse_lazy('product_list')
+
+
+class CategoryListView(ListView):
+    model = ProductCategory
+    template_name = 'category_list.html'
+    context_object_name = 'categories'
+
+
+class CategoryCreateView(CreateView):
+    model = ProductCategory
+    form_class = ProductCategoryForm
+    template_name = 'category_form.html'
+    success_url = reverse_lazy('category_list')
+
+
+class CategoryUpdateView(UpdateView):
+    model = ProductCategory
+    template_name = 'category_form.html'
+    fields = ['name', 'fields']
+    success_url = reverse_lazy('category_list')
