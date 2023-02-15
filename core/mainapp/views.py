a5c035b9d67ccb4e8af591426689ff50c1e984c3
Sblvsk <smr.kolesnikov@gmail.com>
2023-02-15 14:04:58 +0600

    Product views

--- core/mainapp/views.py
+++ core/mainapp/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, DetailView, DeleteView, UpdateView
+from django.urls import reverse_lazy
+from .models import Product, ProductCategory
 
-# Create your views here.
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product_list.html'
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category = self.request.GET.get('category')
+        if category:
+            queryset = queryset.filter(category__name=category)
+        return queryset
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    template_name = 'product_form.html'
+    fields = ['name', 'description', 'price', 'category']
+    success_url = reverse_lazy('product_list')
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'product_confirm_delete.html'
+    success_url = reverse_lazy('product_list')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = 'product_form.html'
+    fields = ['name', 'description', 'price', 'category']
+    success_url = reverse_lazy('product_list')
