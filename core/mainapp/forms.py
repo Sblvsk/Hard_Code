a31174c9d13a959bcc552c1cb3459c782edddfee
Sblvsk <smr.kolesnikov@gmail.com>
2023-02-15 13:48:53 +0600

    forms

---
+++ core/mainapp/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Product, ProductCategory
+
+
+class ProductCategoryForm(forms.ModelForm):
+    class Meta:
+        model = ProductCategory
+        fields = ['name', 'description']
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'price', 'category', 'attributes']
