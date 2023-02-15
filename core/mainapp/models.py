2787e825cccf1f5327cd04b64612044561a9948c
Sblvsk <smr.kolesnikov@gmail.com>
2023-02-15 13:40:57 +0600

    models

--- core/mainapp/models.py
+++ core/mainapp/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from pathlib import Path
+from time import time
 
-# Create your models here.
+
+def products_avatars_path(instance, filename):
+    num = int(time() * 1000)
+    suff = Path(filename).suffix
+    return f"product_{instance.product}/avatars/{f'pic_{num}{suff}'}"
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    delete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductAttribute(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    data_type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = models.PositiveIntegerField()
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to=products_avatars_path, blank=True, null=True)
+    attributes = models.ManyToManyField(ProductAttribute, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
