4cc2fd2f0df87edece1597d1b997f3a871c08c2b
Sblvsk <smr.kolesnikov@gmail.com>
2023-02-16 17:32:05 +0600

    serializers

---
+++ core/api_app/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from mainapp.models import ProductCategory, ProductAttribute, Product
+
+
+class ProductCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductCategory
+        fields = '__all__'
+
+
+class ProductAttributeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductAttribute
+        fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
